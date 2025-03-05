aedb403f762aca340ec42706b0b4c86ddc98269a
AdrainRazini <160901383+AdrainRazini@users.noreply.github.com>
2025-03-05 17:20:18 -0300

    Update and rename Novo Documento RTF.rtf to index.js

---
+++ public/index.js
@@ -0,0 +1,5 @@
+// index.js
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Página carregada com sucesso!');
+  // Aqui você pode adicionar interações ou manipulação de eventos
+});
