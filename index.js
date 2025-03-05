88d171c9a79aa3b7eed146ba6ec1f51f6a985156
AdrainRazini <160901383+AdrainRazini@users.noreply.github.com>
2025-03-05 17:20:32 -0300

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-// index.js
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('Página carregada com sucesso!');
-  // Aqui você pode adicionar interações ou manipulação de eventos
-});
